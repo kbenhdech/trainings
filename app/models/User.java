ab891cc9c9ecf8ae18f94757e3eee252542f52d6
kbenhdech <karim.benhdech@vidal.fr>
2010-07-01 13:54:48 +0200

    Add User class.

---
+++ app/models/User.java
@@ -0,0 +1,33 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.jpa.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kbenhdech
+ * Date: 1 juil. 2010
+ * Time: 13:52:16
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class User extends Model {
+
+    public String email;
+    
+    public String password;
+
+    public String fullname;
+
+    public boolean isAdmin;
+
+    public User(String email, String password, String fullname) {
+        this.email = email;
+        this.password = password;
+        this.fullname = fullname;
+    }
+
+}
+
