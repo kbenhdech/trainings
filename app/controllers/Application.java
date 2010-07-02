a0dd5ea7a6b2400060293de56292814a6c7f8b6b
kbenhdech <karim.benhdech@vidal.fr>
2010-07-02 14:39:36 +0200

    Modify index.html. Add properties in application.conf file. Modify steel sheet.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -15,4 +15,11 @@ public class Application extends Controller {
         render(frontPost, olderPosts);
     }
 
+    @Before
+    static void addDefaults() {
+        renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+        renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+
+
 }
