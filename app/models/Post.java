7d795d9cf8179683393350d1c3d047e1b31f8e53
kbenhdech <karim.benhdech@vidal.fr>
2010-07-02 09:40:50 +0200

    Add Comment model class. Add relation between Post and Comment classes. Add tests.

--- app/models/Post.java
+++ app/models/Post.java
@@ -16,6 +16,7 @@ import play.db.jpa.*;
 public class Post extends Model {
 
     public String title;
+
     public Date postedAt;
 
     @Lob
@@ -24,11 +25,22 @@ public class Post extends Model {
     @ManyToOne
     public User author;
 
+    @OneToMany(mappedBy="post", cascade=CascadeType.ALL)
+    public List<Comment> comments;
+
     public Post(User author, String title, String content) {
+        this.comments = new ArrayList<Comment>();
         this.author = author;
         this.title = title;
         this.content = content;
         this.postedAt = new Date();
     }
+
+    public Post addComment(String author, String content) {
+        Comment newComment = new Comment(this, author, content).save();
+        this.comments.add(newComment);
+        return this;
+    }
+
 
 }
