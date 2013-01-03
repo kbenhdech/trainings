c8fa6518409ed6f4aba8e990e7e7b4d80f07501f
Karim BENHDECH <kbenhdech@Andromeda.(none)>
2013-01-03 20:28:27 +0100

    Modify index view

--- bookmarks/app/controllers/Application.java
+++ bookmarks/app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.Bookmark;
+import models.Category;
 import play.*;
 import play.mvc.*;
 
@@ -8,7 +10,11 @@ import views.html.*;
 public class Application extends Controller {
   
   public static Result index() {
-    return ok(index.render("Application Bookmark"));
+    return ok(index.render(
+            "Vous pouvez commencer à saisir ...",
+            Bookmark.find.fetch("category").orderBy("title").findList(),
+            Category.find.orderBy("label").findList()
+    ));
   }
   
 }
