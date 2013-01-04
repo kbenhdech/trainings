a14a99290678e4816db596ba2d6aa0f9c65a8331
Karim BENHDECH <karim.benhdech@gmx.fr>
2013-01-04 10:47:28 +0100

    Server side validation

--- bookmarks/app/controllers/Categories.java
+++ bookmarks/app/controllers/Categories.java
@@ -10,9 +10,14 @@ public class Categories extends Controller {
     public static Result add() {
 
         final Form<Category> categoryForm = form(Category.class).bindFromRequest();
-        final Category category = categoryForm.get();
 
-        category.save();
+        if (categoryForm.hasErrors()) { // <--- le code modifié, ça commence ici
+            flash("error", "Non, non, il faut saisir autre chose !");
+        } else { // <--- on n'enregistre que si tout va bien
+            final Category category = categoryForm.get();
+            category.save();
+        }
+
         return redirect(routes.Application.index());
 
     }
--- bookmarks/app/models/Category.java
+++ bookmarks/app/models/Category.java
@@ -2,12 +2,15 @@ package models;
 
 import play.db.ebean.Model;
 import javax.persistence.*;
+import play.data.validation.Constraints;
 
 @Entity
 public class Category extends Model{
 
     @Id
     public Long id;
+    @Constraints.Required
+    @Constraints.MaxLength(30)
     public String label;
 
     public static Finder<Long, Category> find =
